860e653cce530fc826deee2f2c0737ee0133a7ea
Allison Carlisle <allisonbcarlisle@gmail.com>
2024-08-05 23:25:06 -0700

    trying again from an old commit

--- backend/dal.js
+++ backend/dal.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
  
 // Replace the following with your Atlas connection string
-const uri = process.env.MONGODB_URI || "mongodb+srv://allisonbcarlisle:areyoukiddingme@threetierbankingapplica.mb9bg5w.mongodb.net/?retryWrites=true&w=majority&appName=ThreeTierBankingApplication";
+const uri = "mongodb+srv://allisonbcarlisle:areyoukiddingme@threetierbankingapplica.mb9bg5w.mongodb.net/?retryWrites=true&w=majority&appName=ThreeTierBankingApplication";
 
 //"mongodb://localhost:27017";
 
